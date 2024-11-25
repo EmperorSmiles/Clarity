22e59717e3d1751534d0112dd5a02feaa82fe2d0
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-25 21:07:44 +0100

    Added Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -5,9 +6,7 @@ function App() {
     <>
       <NavBar />
       <div className="bg-background-light h-screen flex flex-col justify-center items-center font-vietnam">
-        <h1 className="text-text-light text-3xl ">
-          FUCK OFF!! Wait! Are you really gonna leave?? 🥹
-        </h1>
+        <Hero />
       </div>
     </>
   );
