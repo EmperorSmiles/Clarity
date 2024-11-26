b6d5932a2dad170776b486ff08e028b8caed488b
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-26 19:23:10 +0100

    Created Button Comp

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  omClick?: () => void;
+  styleVariant?: "default" | "nav";
+}
+
+const Button = ({
+  children,
+  omClick,
+  styleVariant = "default",
+}: ButtonProps) => {
+  const navStyle =
+    "rounded-xl px-2 py-1 md:px-4 md:py-2 bg-teal-600 hover:bg-teal-700 border-2 border-text-light hover:shadow-md hover:md:shadow-lg text-secondary-light text-md hidden md:block transition-colors duration-20";
+
+  const defaultStyle =
+    "bg-teal-600 hover:bg-teal-700 text-white px-8 py-3 rounded-lg text-lg font-medium transition-colors duration-200 mb-4 ";
+
+  const buttonStyle = styleVariant === "nav" ? navStyle : defaultStyle;
+
+  return (
+    <button className={buttonStyle} onClick={omClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
