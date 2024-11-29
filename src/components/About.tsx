67bea3bd52bf6be2817a2e3d97884042854a93ed
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-29 14:37:11 +0100

    Tweaking things

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,7 @@
-// import sad from "../assets/Health/Sad.jpg";
-// import alley from "../assets/Health/Dark Alley.jpg";
+import sad from "../assets/Health/Sad.jpg";
+import alley from "../assets/Health/Dark Alley.jpg";
+import Yoga from "../assets/Health/Yoga.jpg";
+import Hiking from "../assets/Health/Hiking.jpg";
 
 const About = () => {
   return (
@@ -12,16 +14,29 @@ const About = () => {
         We're committed to fostering a world where mental wellness is
         prioritized and accessible to all.
       </p>
-      {/* <img
-        src={sad}
-        alt="Sad Man"
-        className="h-40 w-40 object-fill absolute top-10 left-4 -rotate-6 rounded-3xl shadow-2xl shadow-black/70"
-      />
-      <img
-        src={alley}
-        alt="Man in a dark alley"
-        className="h-40 w-40 object-fill absolute top-20 right-3 rotate-6 rounded-xl shadow-2xl shadow-black/70"
-      /> */}
+
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4 ">
+        <img
+          src={sad}
+          alt="Sad Man"
+          className="h-40 w-32 md:h-80 md:w-64 rounded-md"
+        />
+        <img
+          src={Yoga}
+          alt="Man in a Yoga Pose"
+          className="h-40 w-32 md:h-80 rounded-md  md:w-64"
+        />
+        <img
+          src={alley}
+          alt="Man in a dark alley"
+          className="h-40 w-32 md:h-80 rounded-md  md:w-64"
+        />
+        <img
+          src={Hiking}
+          alt="Lady Hiking"
+          className="h-40 w-32 md:h-80 rounded-md md:w-64"
+        />
+      </div>
       <h1 className="text-4xl md:text-6xl font-bold mb-4">Our Mission</h1>
     </>
   );
