5909b463366de2b0a6d8999375a9720e3dd5ce79
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-29 12:21:38 +0100

    Separated About

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">Our Mission</h1>
+      <p className="">
+        Clarity is a beacon of hope for those navigating the complexities of
+        mental health.
+      </p>
+      <p>
+        We're committed to fostering a world where mental wellness is
+        prioritized and accessible to all.
+      </p>
+    </>
+  );
+};
+
+export default About;
