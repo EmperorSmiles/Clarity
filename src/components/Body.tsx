202cc7e8de03a9e0ce4b3f35b6fe4601f21c60df
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-29 12:47:59 +0100

    Taking shape

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,22 +1,9 @@
-// import sad from "../assets/Health/Sad.jpg";
-// import alley from "../assets/Health/Dark Alley.jpg";
-
 import About from "./About";
 
 const Body = () => {
   return (
     <section className="h-full relative font-vietnam ">
       {" "}
-      {/* <img
-        src={sad}
-        alt="Sad Man"
-        className="h-40 w-40 object-fill absolute top-10 left-4 -rotate-6 rounded-3xl shadow-2xl shadow-black/70"
-      />
-      <img
-        src={alley}
-        alt="Man in a dark alley"
-        className="h-40 w-40 object-fill absolute top-20 right-3 rotate-6 rounded-xl shadow-2xl shadow-black/70"
-      /> */}
       <div className="flex flex-col items-center justify-center h-full mt-8 text-text-light text-lg md:text-2xl text-center px-4">
         <About />
       </div>
