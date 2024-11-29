a9647daef066721944b33316d31e81976f47b047
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-29 19:44:45 +0100

    Updated

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,10 +12,10 @@ const Button = ({
   styleVariant = "default",
 }: ButtonProps) => {
   const navStyle =
-    "rounded-xl px-2 py-1 md:px-4 md:py-2 bg-teal-600 hover:bg-teal-700 border-2 border-text-light hover:shadow-md hover:md:shadow-lg text-secondary-light text-md hidden md:block transition-colors duration-20";
+    "rounded-xl px-2 py-1 md:px-4 md:py-2 bg-teal-600 hover:bg-teal-700 border-2 border-text-light hover:shadow-md hover:md:shadow-lg text-secondary-light  text-sm md:text-md hidden md:block transition-colors duration-20";
 
   const defaultStyle =
-    "bg-teal-600 hover:bg-teal-700 text-white px-2 md:px-8 py-2 md:py-3  rounded-lg text-sm md:text-lg font-medium transition-colors duration-200 mb-4 ";
+    "bg-teal-600 hover:bg-teal-700 text-white px-2 md:px-8 py-2 md:py-3 rounded-lg text-sm md:text-lg font-medium transition-colors duration-200 mb-4 ";
 
   const buttonStyle = styleVariant === "nav" ? navStyle : defaultStyle;
 
