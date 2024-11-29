3fe1751efbd4a6f85d8b8b0ee89afe69d44d5dae
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-11-29 12:47:35 +0100

    Fleshing it out

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+// import sad from "../assets/Health/Sad.jpg";
+// import alley from "../assets/Health/Dark Alley.jpg";
 
 const About = () => {
   return (
     <>
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Our Mission</h1>
       <p className="">
         Clarity is a beacon of hope for those navigating the complexities of
         mental health.
@@ -12,6 +12,17 @@ const About = () => {
         We're committed to fostering a world where mental wellness is
         prioritized and accessible to all.
       </p>
+      {/* <img
+        src={sad}
+        alt="Sad Man"
+        className="h-40 w-40 object-fill absolute top-10 left-4 -rotate-6 rounded-3xl shadow-2xl shadow-black/70"
+      />
+      <img
+        src={alley}
+        alt="Man in a dark alley"
+        className="h-40 w-40 object-fill absolute top-20 right-3 rotate-6 rounded-xl shadow-2xl shadow-black/70"
+      /> */}
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">Our Mission</h1>
     </>
   );
 };
