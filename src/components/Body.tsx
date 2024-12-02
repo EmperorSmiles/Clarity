24c5a839bca9494ae44b256acea989434b7807d0
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-02 23:44:36 +0100

    Setting Dark Mode

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,7 +3,7 @@ import About from "./About";
 const Body = () => {
   return (
     // bg-background-light dark:bg-background-dark
-    <section className="h-full relative font-vietnam bg-background-light ">
+    <section className="h-full relative font-vietnam bg-background-light dark:bg-background-dark ">
       {" "}
       <div className="flex flex-col items-center justify-center h-full mt- text-text-light text-lg md:text-2xl text-center pt-8 px-4">
         <About />
