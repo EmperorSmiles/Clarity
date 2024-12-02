d0d7b21c6547de93c71b04f06404c7823c0cb37c
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-02 14:06:31 +0100

    Added padding to body - horizontal

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,7 +5,7 @@ const Body = () => {
     // bg-background-light dark:bg-background-dark
     <section className="h-full relative font-vietnam bg-background-light ">
       {" "}
-      <div className="flex flex-col items-center justify-center h-full mt- text-text-light text-lg md:text-2xl text-center pt-8">
+      <div className="flex flex-col items-center justify-center h-full mt- text-text-light text-lg md:text-2xl text-center pt-8 px-4">
         <About />
       </div>
     </section>
