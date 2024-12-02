4fc519ca439e2d46f20b177d34540e35e08396fd
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-02 13:24:13 +0100

    Well...

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-light">
+      <ul>
+        <li>Holla</li>
+
+        <li>Holla</li>
+
+        <li>Holla</li>
+
+        <li>Holla</li>
+
+        <li>Holla</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
