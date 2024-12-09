2bf83742c9af408a699a9c6403de9dbd2cb1b10c
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-09 14:56:44 +0100

    More tweaking

--- src/pages/DonatePage.tsx
+++ src/pages/DonatePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DonateHero from "../components/DonateHero";
+import DonateBody from "../components/DonateBody";
 
 const DonatePage: React.FC = () => {
   return (
-    <div className=" h-screen">
+    <div className="min-h-screen">
       <DonateHero />
+      <DonateBody />
     </div>
   );
 };
