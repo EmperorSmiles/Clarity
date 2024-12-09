573c75fb54d9319b45ba4b71a14aea2ebce90f9f
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-09 14:56:35 +0100

    More tweaking

--- src/components/DonateHero.tsx
+++ src/components/DonateHero.tsx
@@ -9,15 +9,15 @@ const DonateHero = () => {
         <img
           src={hopeful}
           alt="Mental health support"
-          className="w-full h-4/5 object-cover"
+          className="w-full h-full object-cover"
         />
         {/* Dark overlay to improve text readability */}
-        <div className="absolute h-4/5 inset-0 bg-black opacity-50 z-10"></div>
+        <div className="absolute h-full inset-0 bg-black opacity-50 z-10"></div>
       </div>
 
       {/* Content Container */}
-      <div className="relative z-15 flex flex-col items-center justify-center h-4/5 text-white text-center px-4 py-8">
-        <h1 className="text-3xl md:text-4xl mt-8 font-bold mb-6">
+      <div className="relative z-15 flex flex-col items-center justify-center h-full text-white text-center px-4 py-8">
+        <h1 className="text-3xl md:text-4xl mt-16 md:mt-32 font-bold mb-6">
           With you, we can illuminate the way.
         </h1>
         <p className="max-w-2xl text-lg md:text-xl">
