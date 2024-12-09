25a3b53d9dd9429ddadb252ea7e480e3cf558f89
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-09 14:56:26 +0100

    More tweaking

---
+++ src/components/DonateBody.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const DonateBody = () => {
+  return (
+    <div>
+      <div>
+        <h2>Every Contribution Matters</h2>
+        <p>
+          Mental health isn't a luxury—it's a fundamental human right. Your
+          support directly empowers individuals to:
+        </p>
+        {/* <br /> */}
+        <br />
+        <p>- Break the silence surrounding mental health</p>{" "}
+        <p>- Access professional counseling services</p>
+        <p>- Support community mental wellness programs</p>
+        <p>- Provide resources for those struggling in silence</p>
+      </div>
+    </div>
+  );
+};
+
+export default DonateBody;
