5e647ccd8574118ff13ed0ff71a8ef8afb8272f9
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-09 14:14:57 +0100

    Restructured to accommodate  for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,32 @@
 import React from "react";
-import Hero from "./components/Hero";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import heroVideo1 from "./assets/Health/HeroVideo.mp4";
-import heroVideo2 from "./assets/Health/HeroVideo2.mp4";
-import heroImage1 from "./assets/Health/Hero.jpg";
-import heroImage2 from "./assets/Health/Hero2.jpg";
-import videoFallback from "./assets/Health/Hero2.jpg";
-import Body from "./components/Body";
+import HomePage from "./pages/HomePage";
+import DonatePage from "./pages/DonatePage";
+// import Body from "./components/Body";
 import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-  const mediaItems = [
-    {
-      type: "video" as const,
-      url: heroVideo1,
-    },
-    {
-      type: "image" as const,
-      url: heroImage1,
-    },
-    {
-      type: "video" as const,
-      url: heroVideo2,
-    },
-    {
-      type: "image" as const,
-      url: heroImage2,
-    },
-  ];
-
   return (
-    <>
-      <NavBar />
-      <div className="bg-background-dark h-screen flex flex-col justify-center items-center font-vietnam">
+    <Router>
+      <div className="font-vietnam">
+        <NavBar />
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/donate" element={<DonatePage />} />
+      </Routes>
+      <Footer />
+
+      {/* <div className="bg-background-dark h-screen flex flex-col justify-center items-center font-vietnam">
         <Hero
           mediaItems={mediaItems}
           videoFallbackImage={videoFallback}
           interval={7000}
         />
       </div>
-      <Body />
-      <Footer />
-    </>
+      <Body /> */}
+    </Router>
   );
 };
 
