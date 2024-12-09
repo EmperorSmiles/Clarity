b6197d48c12df2a00c68445c198c435be2a4cdd7
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-09 14:17:05 +0100

    Separation of Concerns

---
+++ src/pages/DonatePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DonateHero from "../components/DonateHero";
+
+const DonatePage: React.FC = () => {
+  return (
+    <div className=" h-screen">
+      <DonateHero />
+    </div>
+  );
+};
+
+export default DonatePage;
