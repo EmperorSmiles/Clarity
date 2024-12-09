b1385235b7be06cea2d553b8c18d42a69e3f4ef2
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-09 14:15:56 +0100

    Restructured

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,5 @@
 import About from "./About";
+import Donate from "./DonateHero";
 
 const Body = () => {
   return (
@@ -7,6 +8,7 @@ const Body = () => {
       {" "}
       <div className="flex flex-col items-center justify-center h-full mt- text-text-light text-lg md:text-2xl text-center pt-8 px-4">
         <About />
+        <Donate />
       </div>
     </section>
   );
