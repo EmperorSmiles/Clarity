1b39335df19551ff767178f53b5299d69723c265
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-11 14:49:32 +0100

    Cooking texts

--- src/components/DonateBody.tsx
+++ src/components/DonateBody.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const DonateBody = () => {
   return (
     <div>
-      <div>
-        <h2>Every Contribution Matters</h2>
+      <div className="items-center justify-center h-full text-text-light text-center text-lg md:text-2xl">
+        <h2 className="text-2xl md:text-4xl mb-4 font-semibold">
+          Every Contribution Matters
+        </h2>
         <p>
           Mental health isn't a luxury—it's a fundamental human right. Your
           support directly empowers individuals to:
