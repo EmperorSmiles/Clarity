a88f8ab14714d06592c9ab58d388d0bcc186e060
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2024-12-23 11:53:24 +0100

    ???

--- config.ts
+++ config.ts
@@ -5,6 +5,10 @@ export const config = createConfig({
   chains: [mainnet, sepolia],
   transports: {
     [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [sepolia.id]: http(
+      `eth-sepolia.g.alchemy.com/v2/${
+        import.meta.env.VITE_ALCHEMY_SEPOLIA_API_KEY
+      }`
+    ),
   },
 });
